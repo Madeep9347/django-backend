807d883e65e9393992ed64f530b7db1014d795ce
Madeep Yampalaku <111452217+Madeep9347@users.noreply.github.com>
2025-03-13 14:27:07 +0530

    Update start-container.sh

--- script/start-container.sh
+++ script/start-container.sh
@@ -3,9 +3,9 @@ echo "stop the existing container"
 docker rm -f my-app
 
 echo "login to ecr"
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 590183894437.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 682033487445.dkr.ecr.ap-south-1.amazonaws.com
 echo "pull the image"
-docker pull 590183894437.dkr.ecr.us-east-1.amazonaws.com/django-backend:latest
+docker pull 682033487445.dkr.ecr.ap-south-1.amazonaws.com/django-backend:latest
 
 echo "start the container"
-docker run -d -p 8000:80 --name my-app 590183894437.dkr.ecr.us-east-1.amazonaws.com/django-backend:latest
+docker run -d -p 8000:80 --name my-app 682033487445.dkr.ecr.ap-south-1.amazonaws.com/django-backend:latest
