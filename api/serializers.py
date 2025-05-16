2144d8568dcb87d562f8132d89a8733937691b94
Madeep Yampalaku <111452217+Madeep9347@users.noreply.github.com>
2025-05-16 14:38:37 +0530

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Item, Cart
+from .models import Item, Cart, Order
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,8 +7,17 @@ class ItemSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class CartSerializer(serializers.ModelSerializer):
-    item = ItemSerializer()  # Nesting item details inside cart response
+    item = ItemSerializer()
 
     class Meta:
         model = Cart
         fields = ['id', 'item', 'quantity']
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['id', 'name', 'address', 'pincode', 'mobile', 'created_at']
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        return Order.objects.create(user=user, **validated_data)
